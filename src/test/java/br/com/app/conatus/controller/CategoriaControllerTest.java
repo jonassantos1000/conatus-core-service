2b8f41879edf800e241b97e7a9e477c2275e154e
jonassantos1000 <jonasvale3@hotmail.com>
2023-12-17 21:50:39 -0300

    test: implementado testes automatizados para categorias

--- src/test/java/br/com/app/conatus/controller/CategoriaControllerTest.java
+++ src/test/java/br/com/app/conatus/controller/CategoriaControllerTest.java
@@ -24,6 +24,9 @@ class CategoriaControllerTest extends AbstractControllerTest {
 
 	private static final String RESOURCE = "/categorias";
 	private static StringBuilder path = new StringBuilder();
+	private static final Long ID_CATEGORIA_VALIDO = 1L;
+	private static final Long ID_CATEGORIA_INVALIDO = 0L;
+	
 	
 	@Test
 	@Order(1)
@@ -50,7 +53,51 @@ class CategoriaControllerTest extends AbstractControllerTest {
 
 		assertEquals(HttpStatus.BAD_REQUEST, respostaRequisicao.getStatusCode());
 	}
+	
+	@Test
+	@Order(3)
+	void esperaQueRetorneSucessoNaAlteracaoDaCategoria() {
+		
+		path.append("/").append(ID_CATEGORIA_VALIDO);
+	
+		ParameterizedTypeReference<Void> responseType = new ParameterizedTypeReference<>() {
+		};
 
+		var respostaRequisicao = restTemplate.exchange(path.toString(), HttpMethod.PUT,
+				new HttpEntity<>(gerarDadosCategoriaValido(), getHeader()), responseType);
+
+		assertEquals(HttpStatus.OK, respostaRequisicao.getStatusCode());
+	}
+	
+	@Test
+	@Order(4)
+	void esperaQueRetorneErroNaAlteracaoDaCategoriaComIdInvalido() {
+		
+		path.append("/").append(ID_CATEGORIA_INVALIDO);
+	
+		ParameterizedTypeReference<Void> responseType = new ParameterizedTypeReference<>() {
+		};
+
+		var respostaRequisicao = restTemplate.exchange(path.toString(), HttpMethod.PUT,
+				new HttpEntity<>(gerarDadosCategoriaValido(), getHeader()), responseType);
+
+		assertEquals(HttpStatus.NOT_FOUND, respostaRequisicao.getStatusCode());
+	}
+	
+	@Test
+	@Order(5)
+	void esperaQueRetorneErroNaAlteracaoDoFornecedorComDadosInvalidos() {
+		
+		path.append("/").append(ID_CATEGORIA_VALIDO);
+	
+		ParameterizedTypeReference<Void> responseType = new ParameterizedTypeReference<>() {
+		};
+
+		var respostaRequisicao = restTemplate.exchange(path.toString(), HttpMethod.PUT,
+				new HttpEntity<>(gerarDadosCategoriaInvalido(), getHeader()), responseType);
+
+		assertEquals(HttpStatus.BAD_REQUEST, respostaRequisicao.getStatusCode());
+	}
 	
 	private CategoriaRequest gerarDadosCategoriaValido() {
 		return CategoriaRequest.builder().descricao("Teste Unitario JUNIT").build();
