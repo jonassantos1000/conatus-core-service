9efc97a7465d41d51e0cf9a1c0a1d6ab0f16fd3f
jonassantos1000 <jonasvale3@hotmail.com>
2023-12-24 15:01:29 -0300

    feat: implementado metodo para retornar fornecedor por nome

--- src/main/java/br/com/app/conatus/repositories/FornecedorRepository.java
+++ src/main/java/br/com/app/conatus/repositories/FornecedorRepository.java
@@ -1,5 +1,7 @@
 package br.com.app.conatus.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import br.com.app.conatus.commons.entities.FornecedorEntity;
 @Repository
 public interface FornecedorRepository extends JpaRepository<FornecedorEntity, Long>{
 
+	Page<FornecedorEntity> findByNomeContainingIgnoreCase(String nome, Pageable page);
 }
